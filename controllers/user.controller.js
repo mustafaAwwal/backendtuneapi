fcc107f23dd3d20b644c27676f2d7ce900a4e33b
zeeshan <zshaan470@gmail.com>
2019-07-24 18:27:47 +0500

    sfdds

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -19,7 +19,7 @@ module.exports.login = (req, res) => {
             expiresIn: 10000
         }
     )
-    db.execute(`SELECT * FROM testdb.users WHERE userPassword = "${userData.userpassword}" AND userId = "${userData.id}"` ).then((data) => {
+    db.execute(`SELECT * FROM heroku_cd5497db7ba8561.users WHERE userPassword = "${userData.userpassword}" AND userId = "${userData.id}"` ).then((data) => {
         if(data[0].length > 0){
             res.send({
                 "message":'HURRAH', 
@@ -49,7 +49,7 @@ module.exports.signup = (req, res) => {
         id: req.body.userId
     }
 
-    db.execute(`INSERT INTO testdb.users (userName,userPassword,userEmail,userId) VALUES ("${userData.username}","${userData.userpassword}","${userData.useremail}","${userData.id}");` ).then((data) => {
+    db.execute(`INSERT INTO heroku_cd5497db7ba8561.users (userName,userPassword,userEmail,userId) VALUES ("${userData.username}","${userData.userpassword}","${userData.useremail}","${userData.id}");` ).then((data) => {
         console.log(data);
         
         if(data){
@@ -86,7 +86,7 @@ module.exports.createToken = (req, res) => {
         author: req.body.tokenAuthor,
     }
 
-    db.execute(`INSERT INTO testdb.createtoken (tokenName, tokenSymbol, tokenAuthor, category, decimals, totalSupply, website, youtube, ethAddress, tokenDescription, soundCloud) VALUES ("${userData.name}","${userData.symbol}","${userData.author}","${userData.category}","${userData.decimal}","${userData.supply}","${userData.website}","${userData.youtube}","${userData.ethAddress}","${userData.description}","${userData.soundCloud}");` ).then((data) => {
+    db.execute(`INSERT INTO heroku_cd5497db7ba8561.createtoken (tokenName, tokenSymbol, tokenAuthor, category, decimals, totalSupply, website, youtube, ethAddress, tokenDescription, soundCloud) VALUES ("${userData.name}","${userData.symbol}","${userData.author}","${userData.category}","${userData.decimal}","${userData.supply}","${userData.website}","${userData.youtube}","${userData.ethAddress}","${userData.description}","${userData.soundCloud}");` ).then((data) => {
         console.log(data);
         
         if(data){
@@ -123,7 +123,7 @@ module.exports.listToken = (req, res) => {
         author: req.body.tokenAuthor
     }
 
-    db.execute(`INSERT INTO testdb.listtoken (name, symbol, decimals, ethAddress, description, genre, soundCloud, totalSupply, website, youtube) VALUES ("${userData.name}","${userData.symbol}","${userData.decimal}","${userData.ethAddress}","${userData.description}","${userData.category}","${userData.soundCloud}","${userData.supply}","${userData.website}","${userData.youtube}");` ).then((data) => {
+    db.execute(`INSERT INTO heroku_cd5497db7ba8561.listtoken (name, symbol, decimals, ethAddress, description, genre, soundCloud, totalSupply, website, youtube) VALUES ("${userData.name}","${userData.symbol}","${userData.decimal}","${userData.ethAddress}","${userData.description}","${userData.category}","${userData.soundCloud}","${userData.supply}","${userData.website}","${userData.youtube}");` ).then((data) => {
         console.log(data);
         
         if(data){
@@ -152,7 +152,7 @@ module.exports.insertToken = (req, res) => {
         decimals: req.body.tokenDecimals
     }
 
-    db.execute(`INSERT INTO testdb.tokens (addr, name, fullName, decimals) VALUES ("${tokenData.addr}","${tokenData.name}","${tokenData.fullName}","${tokenData.decimals}");` ).then((data) => {
+    db.execute(`INSERT INTO heroku_cd5497db7ba8561.tokens (addr, name, fullName, decimals) VALUES ("${tokenData.addr}","${tokenData.name}","${tokenData.fullName}","${tokenData.decimals}");` ).then((data) => {
         console.log(data);
         
         if(data){
@@ -174,7 +174,7 @@ module.exports.insertToken = (req, res) => {
 
 module.exports.getAllTokens = (req, res) => {
 
-    db.execute(`SELECT * FROM testdb.createtoken` ).then((data) => {
+    db.execute(`SELECT * FROM heroku_cd5497db7ba8561.createtoken` ).then((data) => {
         if(data[0].length > 0){
             res.send({
                 "message":'HURRAH', 
@@ -197,7 +197,7 @@ module.exports.getAllTokens = (req, res) => {
 
 module.exports.getListTokens = (req, res) => {
 
-    db.execute(`SELECT * FROM testdb.listtoken` ).then((data) => {
+    db.execute(`SELECT * FROM heroku_cd5497db7ba8561.listtoken` ).then((data) => {
         if(data[0].length > 0){
             res.send({
                 "message":'HURRAH', 
@@ -219,7 +219,7 @@ module.exports.getListTokens = (req, res) => {
 
 module.exports.getTokenData = (req, res) => {
 
-    db.execute(`SELECT * FROM testdb.tokens` ).then((data) => {
+    db.execute(`SELECT * FROM heroku_cd5497db7ba8561.tokens` ).then((data) => {
         if(data[0].length > 0){
             res.send({
                 "message":'HURRAH', 
